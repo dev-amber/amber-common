bcb3cf72fbad308b5ef8f0c803986acada796abe
Paweł Suder <pawel@suder.info>
2014-04-26 13:48:02 +0200

    Fix synchronization mechanism

--- sync.sh
+++ sync.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-set -x
+#set -x
 
 export __dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 for file_to_change in $(cd ..; find ./ -name *.proto | grep -v $(basename ${__dir}))
 do
-	cp $(find ${__dir} -name $(basename ${file_to_change})) ../${file_to_change}
+	export file_to_copy=$(find ${__dir} -name $(basename ${file_to_change}))
+	if [ -n "${file_to_copy}" ]
+	then
+		echo "cp ${file_to_copy} ../${file_to_change}"
+		cp ${file_to_copy} ../${file_to_change}
+	fi
 done
