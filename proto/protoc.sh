6c309b71c53c1ff0aca69f09dce809db3e6a3aa1
Paweł Suder <pawel@suder.info>
2014-04-26 12:38:18 +0200

    Add script to create python files from protobuf files, reorganize structure

---
+++ proto/protoc.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export __dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [ "$#" -ne 2 ]
+then
+	echo "Usage: ${0} <type> <out_dir>"
+	echo "Types: python, java, cpp"
+	exit 1
+fi
+
+set -x
+
+export __type=${1}
+export __out_dir=${2}
+
+mkdir -p ${__out_dir}
+protoc -I ${__dir} --${__type}_out=${__out_dir} ${__dir}/drivermsg.proto
+for __module in $(ls ${__dir}/modules)
+do
+    protoc -I ${__dir} --${__type}_out=${__out_dir} ${__dir}/modules/${__module}
+done
